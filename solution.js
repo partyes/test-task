657217c29eca69dcdf9b648b32c156c8a2f068ad
George Komarov <george@192.168.2.177>
2017-09-05 10:55:01 +0300

    First approach

--- solution.js
+++ solution.js
@@ -1,15 +1,59 @@
 function drawNestedSetsTree(data, node) {
-  // Удалите весь код ниже и замените его на свою реализацию
-  const ul = document.createElement('ul');
-  data.forEach(({ title }) => {
-    const li = document.createElement('li');
-    li.textContent = title;
-    ul.appendChild(li);
-  });
-  node.appendChild(ul);
+	const sets = data ? data.slice().sort(({left: l1}, {left: l2}) => l1 - l2 ) : [];
+
+	function traverse(current, previous) {
+		if (!sets[current]) {
+			return;
+		}
+
+		if (undefined === previous) {
+			const ul = document.createElement('ul');
+			node.appendChild(ul);
+
+			const li = document.createElement('li');
+			li.textContent = sets[current].title;
+			ul.appendChild(li);
+
+			sets[current].node = li;
+
+			traverse(current + 1, current);
+
+		} else if (isChildrenOf(sets[current], sets[previous])) {
+			const ul = document.createElement('ul');
+			sets[previous].node.appendChild(ul);
+
+			const li = document.createElement('li');
+			li.textContent = sets[current].title;
+			ul.appendChild(li);
+
+			sets[current].node = li;
+
+			traverse(current + 1, current);				
+
+		} else if (isSiblings(sets[previous], sets[current])) {
+			const li = document.createElement('li');
+			li.textContent = sets[current].title;
+			sets[previous].node.parentNode.appendChild(li);
+
+			sets[current].node  = li;
+
+			traverse(current + 1, current);
+			
+		} else {
+			traverse(current, previous - 1);
+		}
+	}
+
+	traverse(0);
+
+	function isChildrenOf(child, parent) {
+		return parent.left + 1 === child.left;
+	}
+
+	function isSiblings(prev, cur) {
+		return prev.right + 1 === cur.left;
+	}
 }
-
-
 
 if (typeof module !== 'undefined') {
   module.exports = drawNestedSetsTree;
